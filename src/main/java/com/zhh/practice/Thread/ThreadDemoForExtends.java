6cfd45d4b620d3d22cd815e02fd51d7bf4c4bfea
zhu <huanghong0118@126.com>
2021-03-11 18:05:37 +0800

    新增线程demo

---
+++ src/main/java/com/zhh/practice/Thread/ThreadDemoForExtends.java
@@ -0,0 +1,11 @@
+package com.zhh.practice.Thread;
+
+/**
+ * @description:
+ * @see:com.zhh.practice.Thread
+ * @author:zhh
+ * @createTime:2021-03-11 14:35
+ * @version:1.0
+ */
+public class ThreadDemoForExtends {
+}
---
+++ src/main/java/com/zhh/practice/Thread/ThreadDemoForRunnable.java
@@ -0,0 +1,11 @@
+package com.zhh.practice.Thread;
+
+/**
+ * @description:
+ * @see:com.zhh.practice.Thread
+ * @author:zhh
+ * @createTime:2021-03-11 15:00
+ * @version:1.0
+ */
+public class ThreadDemoForRunnable {
+}
