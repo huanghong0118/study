83302838644c4bba4f7d6762b3e81a8364970e92
zhu <huanghong0118@126.com>
2021-03-20 10:59:07 +0800

    新增不安全集合类

---
+++ src/main/java/com/zhh/practice/juc/NoSafeDemo.java
@@ -0,0 +1,11 @@
+package com.zhh.practice.juc;
+
+/**
+ * @description:请举例说明集合类是不安全的
+ * @see:com.zhh.practice.juc
+ * @author:zhh
+ * @createTime:2021-03-19 8:26
+ * @version:1.0
+ */
+public class NoSafeDemo {
+}
