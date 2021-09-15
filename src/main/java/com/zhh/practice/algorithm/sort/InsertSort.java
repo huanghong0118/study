c4d12441684d7e0bd1ba163457067b5a7e1b5b11
zhu <huanghong0118@126.com>
2021-09-15 09:26:19 +0800

    插入排序选择排序

--- src/main/java/com/zhh/practice/algorithm/sort/InsertSort.java
+++ src/main/java/com/zhh/practice/algorithm/sort/InsertSort.java
@@ -1,23 +1,22 @@
 package com.zhh.practice.algorithm.sort;
 
 /**
- * @description:选择排序
+ * @description:插入
  * @see:com.zhh.practice.algorithm.sort
  * @author:zhh
- * @createTime:2021-09-15 8:51
+ * @createTime:2021-09-15 9:01
  * @version:1.0
  */
-public class SelectSort {
+public class InsertSort {
 
-    public static void selectSort(int[] arr){
-        for(int i = arr.length - 1;i > 0;i--){
-            for(int j = 0;j < i;j++){
-                if(arr[j+1] < arr[j]){
-                    swap(arr,j,j+1);
-                }
+    public static void insertSort(int[] arr){
+        for(int i = 0;i < arr.length - 1;i ++){
+            for(int j = i + 1;j > 0 && arr[j] < arr[j-1];j --){
+                swap(arr,j,j-1);
             }
         }
     }
+
     private static void swap(int[] arr,int i,int j){
         arr[i] = arr[i] ^ arr[j];
         arr[j] = arr[i] ^ arr[j];
@@ -26,7 +25,7 @@ public class SelectSort {
 
     public static void main(String[] args) {
         int[] array = new int[]{1,7,5,10,9,8,4};
-        selectSort(array);
+        insertSort(array);
         System.out.println(array);
     }
 }
